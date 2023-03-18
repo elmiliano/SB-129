b8f82b9e066576545f27f04b0e55cae68c7ab228
Emiliano Machado <emiliano.a.machado@gmail.com>
2023-03-17 18:20:43 -0700

    Added text indicators to screens

--- sb129/lib/screens/humidity_screen.dart
+++ sb129/lib/screens/humidity_screen.dart
@@ -5,6 +5,6 @@ class HumidityScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Text('Humidity Screen');
   }
 }
--- sb129/lib/screens/temp_screen.dart
+++ sb129/lib/screens/temp_screen.dart
@@ -5,6 +5,6 @@ class TempScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Text('Temperature Screen');
   }
 }
