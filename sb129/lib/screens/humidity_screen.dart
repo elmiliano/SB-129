67e18f24aadaa63ba958a9ebc913d1c814fac0b1
Emiliano Machado <emiliano.a.machado@gmail.com>
2023-03-17 17:55:30 -0700

    Created Humidity Screen file

---
+++ sb129/lib/screens/humidity_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HumidityScreen extends StatelessWidget {
+  const HumidityScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
