a122e3e91ec15f2dd3e2e92c77e1551b1ec90dc9
Emiliano Machado <emiliano.a.machado@gmail.com>
2023-03-17 18:17:15 -0700

    Fixed Bottom Navigation Bar functionality

--- sb129/lib/screens/navbar_screen.dart
+++ sb129/lib/screens/navbar_screen.dart
@@ -1,25 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:sb129/screens/screens.dart';
 
-class NavbarScreen extends StatelessWidget {
+class NavbarScreen extends StatefulWidget {
   NavbarScreen({super.key});
 
-  List<Widget> screens = [
+  @override
+  State<NavbarScreen> createState() => _NavbarScreenState();
+}
+
+class _NavbarScreenState extends State<NavbarScreen> {
+  void changeScreen(index) {
+    num diff = pageIndex - index;
+    pageIndex = index;
+
+    if ((diff).abs() > 1) {
+      pageController.jumpToPage(index);
+    } else {
+      pageController.animateToPage(index,
+          duration: const Duration(milliseconds: 500), curve: Curves.easeOut);
+    }
+    setState(() {});
+  }
+
+  int pageIndex = 0;
+  PageController pageController = PageController();
+
+  final pages = [
     const TempScreen(),
     const HumidityScreen(),
     const GraphScreen()
   ];
+
   int index = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Navbar Screen')),
-      body: Column(),
-      bottomNavigationBar: NavigationBar(
-        destinations: screens,
-        selectedIndex: index,
-      ),
-    );
+        appBar: AppBar(title: const Text('Navbar Screen')),
+        body: SizedBox.expand(
+          child: PageView(
+            controller: pageController,
+            onPageChanged: changeScreen,
+            children: pages,
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+                label: "temperature", icon: Icon(Icons.thermostat_sharp)),
+            BottomNavigationBarItem(
+                label: "humidity", icon: Icon(Icons.water_drop_rounded)),
+            BottomNavigationBarItem(
+                label: "graph", icon: Icon(Icons.auto_graph_outlined)),
+          ],
+          currentIndex: pageIndex,
+          onTap: (int index) {
+            changeScreen(index);
+          },
+        ));
   }
 }
