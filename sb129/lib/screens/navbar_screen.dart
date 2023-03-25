afb7a10ae35f9f628e8ea9ba59ecdcc46b8d2057
Juan Barrera <this.is.juanb@gmail.com>
2023-03-24 17:39:51 -0700

    tempscreen y navbar_screen parte superior

--- sb129/lib/screens/navbar_screen.dart
+++ sb129/lib/screens/navbar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:sb129/screens/screens.dart';
 
 class NavbarScreen extends StatefulWidget {
@@ -36,15 +37,67 @@ class _NavbarScreenState extends State<NavbarScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Navbar Screen')),
-        body: SizedBox.expand(
-          child: PageView(
-            controller: pageController,
-            onPageChanged: changeScreen,
-            children: pages,
-          ),
+        // appBar: AppBar(title: const Text('Navbar Screen')),
+        body: Column(
+          children: [
+            Container(
+              height: 10,
+              color: Color.fromRGBO(228, 228, 228, 100),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: double.infinity,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 66,
+                      width: 66,
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              bottomLeft: Radius.circular(10)),
+                          color: Color.fromRGBO(106, 213, 203, 100)),
+                    ),
+                    Expanded(
+                      child: Container(
+                        height: 66,
+                        decoration: const BoxDecoration(
+                            color: Color.fromRGBO(228, 228, 228, 100)),
+                        child: Text(
+                          "temperatura",
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.raleway(
+                              fontSize: 48,
+                              color: const Color.fromRGBO(139, 139, 139, 100)),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: 66,
+                      width: 66,
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(10),
+                              bottomRight: Radius.circular(10)),
+                          color: Color.fromRGBO(106, 213, 203, 100)),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: PageView(
+                controller: pageController,
+                onPageChanged: changeScreen,
+                children: pages,
+              ),
+            ),
+          ],
         ),
         bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Color.fromRGBO(106, 213, 203, 100),
           items: const [
             BottomNavigationBarItem(
                 label: "temperature", icon: Icon(Icons.thermostat_sharp)),
--- sb129/lib/screens/temp_screen.dart
+++ sb129/lib/screens/temp_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
 class TempScreen extends StatelessWidget {
@@ -5,6 +7,63 @@ class TempScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text('Temperature Screen');
+    return Scaffold(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 262,
+              width: double.infinity,
+              decoration:
+                  const BoxDecoration(color: Color.fromRGBO(73, 109, 219, 100)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    Icons.thermostat_outlined,
+                    color: Colors.red,
+                    size: 95,
+                  ),
+                  Text(
+                    "15.82°C",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.staatliches(
+                        fontSize: 48, color: Colors.white),
+                  ),
+                  Text(
+                    "Temperatura Ambiente\n16.2°C/24.16°C",
+                    textAlign: TextAlign.center,
+                    style:
+                        GoogleFonts.raleway(fontSize: 13, color: Colors.white),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Text(
+            "detalles",
+            style: GoogleFonts.raleway(fontSize: 16, color: Colors.black),
+          ),
+          Row(
+            children: [
+              Container(
+                height: 99,
+                width: 100,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black, //color of border
+                    width: 1, //width of border
+                  ),
+                  borderRadius: const BorderRadius.all(Radius.circular(10)),
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+      backgroundColor: Colors.white,
+    );
   }
 }
