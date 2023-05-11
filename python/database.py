8343aada9d0666e24cd9e36fa7ba4671a9190b34
Emiliano Machado <emiliano.a.machado@gmail.com>
2023-05-10 18:39:53 -0700

    Set y Update de datos firebase de python

--- python/database.py
+++ python/database.py
@@ -2,7 +2,24 @@ import firebase_admin # IF NOT DEFINED --IN TERMINAL--> pip3 install firebase-ad
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("/SB-129/python/sb-129-proj-f1b72e0e0f19.json")
+path = '/Users/emilianomachado/Documents/CETYS/8to Semestre/Proyecto ICE/Interfaz GitHub/SB-129/python/sb-129-proj-f1b72e0e0f19.json'
+
+cred = credentials.Certificate(path)
+print(cred)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client() # connecting to firestore
+
+collection = db.collection('temp_test')  # create collection
+res = collection.document('A01').set({ # insert document
+    'temp'     : 20,
+    'humedad'  : 30,
+    'puerta1'  : 1, #OPEN
+    'puerta2'  : 0  #CLOSED
+})
+
+res = collection.document('A01').update({ # insert document
+    'temp'     : 46,
+    'humedad'  : 32,
+    'puerta2'  : 1
+})
