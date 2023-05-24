a8d807a63041be1b728d67879c4cd9e11f13af68
Emiliano Machado <emiliano.a.machado@gmail.com>
2023-05-23 17:00:04 -0700

    Conexión entre python y firebase con realtime db

--- python/database.py
+++ python/database.py
@@ -1,25 +1,19 @@
 import firebase_admin # IF NOT DEFINED --IN TERMINAL--> pip3 install firebase-admin
+
 from firebase_admin import credentials
 from firebase_admin import firestore
+from firebase_admin import db
 
-path = '/Users/emilianomachado/Documents/CETYS/8to Semestre/Proyecto ICE/Interfaz GitHub/SB-129/python/sb-129-proj-f1b72e0e0f19.json'
+import json
+
+path = 'python/sb-129-proj-f1b72e0e0f19.json'
+databaseURL = 'https://sb-129-proj-default-rtdb.firebaseio.com/'
 
 cred = credentials.Certificate(path)
-print(cred)
-firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app(cred, {'databaseURL': databaseURL})
 
-db = firestore.client() # connecting to firestore
+ref = db.reference("/")
 
-collection = db.collection('temp_test')  # create collection
-res = collection.document('A01').set({ # insert document
-    'temp'     : 20,
-    'humedad'  : 30,
-    'puerta1'  : 1, #OPEN
-    'puerta2'  : 0  #CLOSED
-})
-
-res = collection.document('A01').update({ # insert document
-    'temp'     : 46,
-    'humedad'  : 32,
-    'puerta2'  : 1
-})
+with open("python/data.json", "r") as file:
+	file_contents = json.load(file)
+ref.set(file_contents)
