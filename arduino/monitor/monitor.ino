7c7089a8d5e74c7a28b8e655b5ffd90d4ea6370a
Machado, E <emiliano.a.machado@gmail.com>
2023-05-27 22:35:37 -0700

    Notas agregadas a monitor.ino

--- arduino/monitor/monitor.ino
+++ arduino/monitor/monitor.ino
@@ -1,3 +1,6 @@
+// Correr en Arduino IDE, seguir este link -> 
+// https://www.circuitbasics.com/how-to-set-up-the-dht11-humidity-sensor-on-an-arduino/?fbclid=IwAR0y2C-RkUd0NE2oHNovG4evrQzV3PZTprS6Q1berOfZjEyEn6uk5G4kdr8
+
 #include <dht.h>
 
 dht DHT;
